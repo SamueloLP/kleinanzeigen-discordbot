da8fdbd28a1884976f2ccec89f0df177e875a92b
SamueloLP <SamueloLP@web.de>
2024-08-08 13:34:46 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,30 @@
 import discord
 from discord.ext import commands
+import subprocess
 import asyncio
 
 intents = discord.Intents.default()
 intents.messages = True
-intents.message_content = True
+intents.message_content = True  # Enable the message content intent
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+class MyBot(commands.Bot):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    async def setup_hook(self):
+        await self.load_extension('link_monitor')
+        await bot.tree.sync()  # Synchronize the commands with Discord
+
+bot = MyBot(command_prefix=commands.when_mentioned_or(''), intents=intents)
+
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}!')
+    print(f'Bot is ready and commands are synchronized.')
 
 async def main():
     async with bot:
-        await bot.load_extension('link_monitor')        # loads "link_monitor.py"
-        await bot.start('token')                        # Insert Bot-Token here
+        await bot.start('token')	# Discord Token
 
 if __name__ == "__main__":
     asyncio.run(main())
